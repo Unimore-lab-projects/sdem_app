7a271c3cd32a50ab6a2ba48bfdb4dff9fa1a908a
Francesco-Prandini <74424@studenti.unimore.it>
2016-03-31 15:36:24 +0200

    Modificato metodo nativo detectAndDrawMarkersJNI

--- app/src/main/jni/SdemAppJNI.cpp
+++ app/src/main/jni/SdemAppJNI.cpp
@@ -1,6 +1,8 @@
 #include <jni.h>
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
+//#include <detection.h>
 #include "detection.h"
 
 
@@ -8,40 +10,54 @@
 
 using namespace cv;
 
-JNIEXPORT void JNICALL
-Java_sdem_unimore_com_sdemapp_CameraView_detectAndDrawMarkersJNI(JNIEnv *env, jobject instance,
-                                                                 jbyteArray data_, jint height_,
-                                                                 jint width_) {
-    jbyte *data = env->GetByteArrayElements(data_, NULL);
-    int h = (int) height_;
-    int w = (int) width_;
-    Mat *m = new Mat(height_, width_, CV_8UC3, data, CV_AUTO_STEP);
-    detection_marker *dm = new detection_marker(DICT_6X6_250);
-    dm->detect(*m);
-
-    env->ReleaseByteArrayElements(data_, data, 0);
-}
-
 extern "C" {
 JNIEXPORT void JNICALL
 Java_sdem_unimore_com_sdemapp_CameraView_provaJNI(JNIEnv *env, jobject instance, jbyteArray data_) {
 
     int i = 0;
 
-    jbyte buf = 0, prevByte = 0;
-    jbyte *data = env->GetByteArrayElements(data_, NULL);
+    jbyte buf = 0,prevByte = 0;
+    jbyte *data = env->GetByteArrayElements( data_, NULL);
     jsize length = env->GetArrayLength(data_);
 
-    for (i = 0; i < length; i++) {
+    for(i=0;i<length;i++){
         //buf=data[i];
         //data[i]=data[i]|prevByte;
         //prevByte=buf;
-        if (data[i] > 127) data[i] = data[i] - 127;
-        else if (data[i] < 127) data[i] = data[i] + 127;
+        if(data[i]>127) data[i]=data[i]-127;
+        else if(data[i]<127) data[i]=data[i]+127;
     }
+
+    env->ReleaseByteArrayElements( data_, data, 0);
+}
+
+JNIEXPORT void JNICALL
+Java_sdem_unimore_com_sdemapp_CameraView_detectAndDrawMarkersJNI(JNIEnv *env, jobject instance,
+                                                                 jbyteArray data_, jint height_, jint width_) {
+
+    //int i=0;
+
+    jbyte *data = env->GetByteArrayElements(data_, NULL);
+    int h=(int) height_;
+    int w=(int) width_;
+    Mat *m = new Mat(height_,width_,CV_8UC3,data,CV_AUTO_STEP);
+    detection_marker *dm=new detection_marker(DICT_6X6_250);
+    dm->detectAndDraw(*m);
+
+   /* jsize length = env->GetArrayLength(data_);
+
+    for(i=0;i<length;i++){
+        //buf=data[i];
+        //data[i]=data[i]|prevByte;
+        //prevByte=buf;
+        if(data[i]>127) data[i]=data[i]-127;
+        else if(data[i]<127) data[i]=data[i]+127;
+    }*/
+
 
     env->ReleaseByteArrayElements(data_, data, 0);
 }
+
 
 
 };
