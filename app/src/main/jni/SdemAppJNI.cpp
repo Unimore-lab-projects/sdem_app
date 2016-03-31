bda3d0fb5a526c50518c3071b49d7c354cd3d19c
Francesco-Prandini <74424@studenti.unimore.it>
2016-03-31 17:42:01 +0200

    Modificato metodo nativo detectAndDrawMarkersJNI

--- app/src/main/jni/SdemAppJNI.cpp
+++ app/src/main/jni/SdemAppJNI.cpp
@@ -1,8 +1,18 @@
 #include <jni.h>
 
+#include <opencv/cv.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc_c.h>
-//#include <detection.h>
+//#include <detection.h>#include <iostream>
+//#include "opencv2/ts.hpp"
+//#include "opencv2/core/private.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+
+#include "opencv2/imgproc/imgproc_c.h"
+
+
 #include "detection.h"
 
 
@@ -37,22 +47,14 @@ Java_sdem_unimore_com_sdemapp_CameraView_detectAndDrawMarkersJNI(JNIEnv *env, jo
 
     //int i=0;
 
+
     jbyte *data = env->GetByteArrayElements(data_, NULL);
     int h=(int) height_;
     int w=(int) width_;
-    Mat *m = new Mat(height_,width_,CV_8UC3,data,CV_AUTO_STEP);
+    Mat *m = new Mat(height_+height_/2,width_,CV_8UC1,(uchar*)data);
+    cv::cvtColor(*m,*m,CV_YUV2RGB_NV21);
     detection_marker *dm=new detection_marker(DICT_6X6_250);
     dm->detectAndDraw(*m);
-
-   /* jsize length = env->GetArrayLength(data_);
-
-    for(i=0;i<length;i++){
-        //buf=data[i];
-        //data[i]=data[i]|prevByte;
-        //prevByte=buf;
-        if(data[i]>127) data[i]=data[i]-127;
-        else if(data[i]<127) data[i]=data[i]+127;
-    }*/
 
 
     env->ReleaseByteArrayElements(data_, data, 0);
--- app/src/main/jni/detection.h
+++ app/src/main/jni/detection.h
@@ -14,27 +14,24 @@ using namespace aruco;
 using  namespace std;
 
 
+
 class detection_marker {
 
-    Ptr<Dictionary> _dictionary;
-    // use: (DICT_6X6_250);
-    vector<int> _markerIds;
-    vector<vector<Point2f>> _markerCorners;
-    Mat inImage;
+	Ptr<Dictionary> _dictionary;// use: (DICT_6X6_250);
+	vector<int> _markerIds;
+	vector<vector<Point2f>> _markerCorners;
+	Mat inImage;
 
 public:
 
-    detection_marker(PREDEFINED_DICTIONARY_NAME name);
+	detection_marker(PREDEFINED_DICTIONARY_NAME name);
 
-    const vector<int> &ids() const;
+	const vector<int>& ids() const;
+	const vector<vector<Point2f>>& corners() const;
+	const Mat& img() const;
 
-    const vector<vector<Point2f>> &corners() const;
-
-    const Mat &img() const;
-
-    void detectAndDraw(Mat &inImage);
-
-    void detect(Mat &inImage);
+	void detectAndDraw(Mat& inImage);
+	void detect(Mat& inImage);
 
 };
 
